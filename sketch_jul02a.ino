81ad0f5882f1c722436b507f27fd2919bc03497e
Park Ju Hyung <qkrwngud825@gmail.com>
2017-07-25 18:26:22 +0900

    tmp
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- sketch_jul02a.ino
+++ sketch_jul02a.ino
@@ -5,129 +5,168 @@
 #include <LedControl.h>
 #include "font.h"
 
-const int numDevices = 4;      // number of MAX7219s used
-const long scrollDelay = 50;   // adjust scrolling speed
+const int numDevices = 4;	// number of MAX7219s used
+const long scrollDelay = 50;	// adjust scrolling speed
 
 const int motorPin = 12;
 
-unsigned long bufferLong [16] = {0}; 
+unsigned long bufferLong[16] = { 0 };
 
-LedControl lc = LedControl(5,7,6,numDevices);
+LedControl lc = LedControl(5, 7, 6, numDevices);
 
-void vib() {
-    digitalWrite(motorPin, HIGH);
-    delay(250);
-    digitalWrite(motorPin, LOW);
-    delay(200);
-    digitalWrite(motorPin, HIGH);
-    delay(250);
-    digitalWrite(motorPin, LOW);
-    //delay(200);
+void vib()
+{
+	digitalWrite(motorPin, HIGH);
+	delay(250);
+	digitalWrite(motorPin, LOW);
+	delay(100);
+	digitalWrite(motorPin, HIGH);
+	delay(250);
+	digitalWrite(motorPin, LOW);
+	//delay(200);
 }
 
-void setup(){
-    for (int x=0; x<numDevices; x++){
-        lc.shutdown(x,false);       //The MAX72XX is in power-saving mode on startup
-        lc.setIntensity(x,10);       // Set the brightness to default value
-        lc.clearDisplay(x);         // and clear the display
-    }
-    pinMode(motorPin, OUTPUT);
+void setup()
+{
+	for (int x = 0; x < numDevices; x++) {
+		lc.shutdown(x, false);	//The MAX72XX is in power-saving mode on startup
+		lc.setIntensity(x, 10);	// Set the brightness to default value
+		lc.clearDisplay(x);	// and clear the display
+	}
+	pinMode(motorPin, OUTPUT);
 
-    //Serial.begin(9600);
+	//Serial.begin(9600);
 }
 
-void loop(){
-        
-    scrollMessage();
-    // scrollFont();
+void loop()
+{
+  vib();
+	showMessage(false);
+	// scrollFont();
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-int trim_matrix(const unsigned char* font) {
-    int i;
-    int j;
-    const unsigned char product = 1;
-    int ret = 0;
-    unsigned char temp;
+int trim_matrix(const unsigned char *font)
+{
+	int i;
+	int j;
+	const unsigned char product = 1;
+	int ret = 0;
+	unsigned char temp;
 
-    for (i = 0; i < 8; i++) {
-        temp = pgm_read_byte_near((font + i));
-        for (j = 0; j < 8; j++) {
-            if ((product & temp) == 1) {
-                if (ret < (8 - j)) { // 8-j == length
-                   ret = 8 - j;
-                }
-                break;
-            }
-            temp = (temp >> 1);
-        }
-    }
+	for (i = 0; i < 8; i++) {
+		temp = pgm_read_byte_near((font + i));
+		for (j = 0; j < 8; j++) {
+			if ((product & temp) == 1) {
+				if (ret < (8 - j)) {	// 8-j == length
+					ret = 8 - j;
+				}
+				break;
+			}
+			temp = (temp >> 1);
+		}
+	}
 
-    if (ret == 0)
-        return 3; // Space
-    
-    return ret + 1;
+	if (ret == 0)
+		return 3;	// Space
+
+	return ret + 1;
 }
 
-// Show Message without scrolling
-void staticMessage() {
-    int fontsize = sizeof(font5x7) / 8;
-    for (int counter = 0; counter < fontsize; counter++){
-        loadBufferLong(counter, true);
-    }
-}
-
-// Scroll Message
-void scrollMessage() {
-    int fontsize = sizeof(font5x7) / 8;
-    for (int counter = 0; counter < fontsize; counter++){
-        loadBufferLong(counter, false);
-    }
+// Show Message
+void showMessage(bool still)
+{
+	int fontsize = sizeof(font5x7) / 8;
+	for (int counter = 0; counter < fontsize; counter++) {
+		loadBufferLong(counter, still);
+	}
+  if (still) {
+    printBufferLong();
+    delay(4294967295);
+  }
 }
 
 // Load character into scroll buffer
-void loadBufferLong(int offset, bool still) {
-        for (int a=0;a<8;a++){                      // Loop 7 times for a 5x7 font
-            unsigned long c = pgm_read_byte_near(font5x7 + (offset * 8) + a);     // Index into character table to get row data
-            unsigned long x = bufferLong [a*2];     // Load current scroll buffer
-            x = x | c;                              // OR the new character onto end of current
-            bufferLong [a*2] = x;                   // Store in buffer
-        }
-        //byte count = pgm_read_byte_near(font5x7 +((ascii - 0x20) * 9) + 8);     // Index into character table for kerning data
-        int count = trim_matrix(font5x7 + (offset * 8));
-        //byte count = 6;
-        for (int x=0; x<count;x++){
-            rotateBufferLong();
-            printBufferLong();
-            delay(scrollDelay);
-        }
-}
-// Rotate the buffer
-void rotateBufferLong(){
-    for (int a=0;a<8;a++){                      // Loop 7 times for a 5x7 font
-        unsigned long x = bufferLong [a*2];     // Get low buffer entry
-        byte b = bitRead(x,31);                 // Copy high order bit that gets lost in rotation
-        x = x<<1;                               // Rotate left one bit
-        bufferLong [a*2] = x;                   // Store new low buffer
-        x = bufferLong [a*2+1];                 // Get high buffer entry
-        x = x<<1;                               // Rotate left one bit
-        bitWrite(x,0,b);                        // Store saved bit
-        bufferLong [a*2+1] = x;                 // Store new high buffer
+void loadBufferLong(int offset, bool still)
+{
+  int a;
+  unsigned long c, x;
+  // Center align code
+  int skip = 0, skip_offset;
+ for (a = 0; a < 8; a++) {
+    if (pgm_read_byte_near(font5x7 + (offset * 8) + a) == 0) {
+      skip++;
+    } else {
+      skip_offset = a;
+      break;
     }
-}  
-// Display Buffer on LED matrix
-void printBufferLong(){
-  for (int a=0;a<8;a++){                    // Loop 7 times for a 5x7 font
-    unsigned long x = bufferLong [a*2+1];   // Get high buffer entry
-    byte y = x;                             // Mask off first character
-    lc.setRow(3,a,y);                       // Send row to relevent MAX7219 chip
-    x = bufferLong [a*2];                   // Get low buffer entry
-    y = (x>>24);                            // Mask off second character
-    lc.setRow(2,a,y);                       // Send row to relevent MAX7219 chip
-    y = (x>>16);                            // Mask off third character
-    lc.setRow(1,a,y);                       // Send row to relevent MAX7219 chip
-    y = (x>>8);                             // Mask off forth character
-    lc.setRow(0,a,y);                       // Send row to relevent MAX7219 chip
   }
+ for (a = 7; a >= 0; a--) {
+    if (pgm_read_byte_near(font5x7 + (offset * 8) + a) == 0)
+      skip++;
+    else
+      break;
+  }
+  skip /= 2;
+  for (a = 0; a < skip; a++) {
+    c = 0;
+    x = bufferLong[a * 2];  // Load current scroll buffer
+    x = x | c;  // OR the new character onto end of current
+    bufferLong[a * 2] = x;  // Store in buffer
+  }
+ for (a = skip; a < 8 - skip_offset + 1; a++) { // Loop 7 times for a 5x7 font
+    c = pgm_read_byte_near(font5x7 + (offset * 8) + a - skip + skip_offset); // Index into character table to get row data
+    x = bufferLong[a * 2];  // Load current scroll buffer
+    x = x | c;  // OR the new character onto end of current
+    bufferLong[a * 2] = x;  // Store in buffer
+  }
+  for (a = 8 - skip_offset + 1; a < 8; a++) {
+    c = 0;
+    x = bufferLong[a * 2];  // Load current scroll buffer
+    x = x | c;  // OR the new character onto end of current
+    bufferLong[a * 2] = x;  // Store in buffer
+  }
+	//byte count = pgm_read_byte_near(font5x7 +((ascii - 0x20) * 9) + 8);     // Index into character table for kerning data
+	int count = trim_matrix(font5x7 + (offset * 8));
+	//byte count = 6;
+	for (a = 0; a < count; a++) {
+		rotateBufferLong();
+    if (!still) {
+		  printBufferLong();
+		  delay(scrollDelay);
+    }
+	}
+}
+
+// Rotate the buffer
+void rotateBufferLong()
+{
+	for (int a = 0; a < 8; a++) {	// Loop 7 times for a 5x7 font
+		unsigned long x = bufferLong[a * 2];	// Get low buffer entry
+		byte b = bitRead(x, 31);	// Copy high order bit that gets lost in rotation
+		x = x << 1;	// Rotate left one bit
+		bufferLong[a * 2] = x;	// Store new low buffer
+		x = bufferLong[a * 2 + 1];	// Get high buffer entry
+		x = x << 1;	// Rotate left one bit
+		bitWrite(x, 0, b);	// Store saved bit
+		bufferLong[a * 2 + 1] = x;	// Store new high buffer
+	}
+}
+
+// Display Buffer on LED matrix
+void printBufferLong()
+{
+	for (int a = 0; a < 8; a++) {	// Loop 7 times for a 5x7 font
+		unsigned long x = bufferLong[a * 2 + 1];	// Get high buffer entry
+		byte y = x;	// Mask off first character
+		lc.setRow(3, a, y);	// Send row to relevent MAX7219 chip
+		x = bufferLong[a * 2];	// Get low buffer entry
+		y = (x >> 24);	// Mask off second character
+		lc.setRow(2, a, y);	// Send row to relevent MAX7219 chip
+		y = (x >> 16);	// Mask off third character
+		lc.setRow(1, a, y);	// Send row to relevent MAX7219 chip
+		y = (x >> 8);	// Mask off forth character
+		lc.setRow(0, a, y);	// Send row to relevent MAX7219 chip
+	}
 }
