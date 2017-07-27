bd7d0a237e0fc5565bfdd9182b7b8c1bc6c2d527
Park Ju Hyung <qkrwngud825@gmail.com>
2017-07-27 17:06:21 +0900

    workaround
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- sketch_jul02a.ino
+++ sketch_jul02a.ino
@@ -2,6 +2,9 @@
 // based on an orginal sketch by Arduino forum member "danigom"
 // http://forum.arduino.cc/index.php?action=profile;u=188950
 
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_PN532.h>
 #include <avr/pgmspace.h>
 #include <LedControl.h>
 #include "font.h"
@@ -16,6 +19,10 @@ const int motorPin = 12;
 unsigned long bufferLong[16] = { 0 };
 
 LedControl lc = LedControl(5, 7, 6, numDevices);
+
+#define PN532_IRQ   (9) // (2)  <- changed to D9 for IRQ pin, refer to schematic
+#define PN532_RESET (8) // (3)  <- changed to D8 for nReset pin, refer to schematic
+Adafruit_PN532 nfc(PN532_IRQ, PN532_RESET);
 
 void vib()
 {
@@ -45,42 +52,141 @@ void setup()
 	Serial.begin(115200);
     //LoRa.setTimeout(500);
   LoRa.begin(38400);
+
+    nfc.begin();
+ 
+    uint32_t versiondata = nfc.getFirmwareVersion();
+    if (! versiondata)
+    {
+        Serial.print("\r\nDidn't find PN53x board");
+        while (1); // halt
+    }
+ 
+    // Got ok data, print it out!
+    Serial.print("\r\nFound chip PN5");
+    Serial.print((versiondata>>24) & 0xFF, HEX);
+    Serial.print("\r\nFirmware ver. ");
+    Serial.print((versiondata>>16) & 0xFF, DEC);
+    Serial.print('.');
+    Serial.print((versiondata>>8) & 0xFF, DEC);
+    
+    nfc.setPassiveActivationRetries(0xFF);
+ 
+    nfc.SAMConfig();
+ 
+    Serial.print("\r\nWaiting for an ISO14443A card");
 }
 
 void showMessage(bool still, const unsigned char* PROGMEM txt, int size);
 void clearScreen(void);
+void nfcReader(void);
 
 void loop()
 {
-  while (LoRa.available()) {
-    String s = LoRa.ReadLine();
-    String m = LoRa.getMessage();
+    if (LoRa.available() > 0) {
+      String s;
+      String m;
 
-    Serial.print("LoRa : ");
-    Serial.print(s);
-    Serial.print("\n");
-    Serial.print("LoRa Message : ");
-    Serial.print(m);
-    Serial.print("\n");
+      for (;;) {
+        Serial.println("Looping");
+        
+        s = LoRa.ReadLine();
+        m = LoRa.getMessage();
+        if (s.length()) {
+          Serial.print("LoRa : \'");
+          Serial.print(s);
+          Serial.print("\'\n");
+        }
+        if (m.length()) {
+          Serial.print("LoRa message : \'");
+          Serial.print(m);
+          Serial.print("\'\n");
+        }
+    
+        if (m == "0001") {
+          setLPM(false);
+          vib();
+          showMessage(false, txt1, sizeof(txt1) / 8);
+          clearScreen();
+        } else if (m == "0002") {
+          setLPM(false);
+          vib();
+          showMessage(false, txt2, sizeof(txt2) / 8);
+          clearScreen();
+        } else if (m == "0003") {
+          setLPM(false);
+          vib();
+          showMessage(false, txt3, sizeof(txt3) / 8);
+          clearScreen();
+        }
 
-	if (m == "0001") {
-      setLPM(false);
-      vib();
-      showMessage(false, txt1, sizeof(txt1) / 8);
-      clearScreen();
-    } else if (m == "0002") {
-	  setLPM(false);
-	  vib();
-	  showMessage(false, txt2, sizeof(txt2) / 8);
-	  clearScreen();
-	} else if (m == "0003") {
-	  setLPM(false);
-	  vib();
-	  showMessage(false, txt3, sizeof(txt3) / 8);
-	  clearScreen();
-	}
-  }
-	// scrollFont();
+        if (s.startsWith("OnRadioTxDone") || s.startsWith("PrepareRxDoneAbort")) {
+          while (LoRa.available() > 0)
+            LoRa.read();
+          LoRa.flush();
+          
+          break;
+        }
+
+        delay(10);
+      }
+        Serial.println("Loop exited");
+    }
+      
+    nfcReader();
+}
+
+void nfcReader(void)
+{
+    boolean success = false;
+    uint8_t uid[] = { 0, 0, 0, 0, 0, 0, 0 };
+    uint8_t uidLength;
+    char arr[2];
+    char temparr[18]={0};
+    temparr[17]='\0';
+    temparr[0]='0';
+    temparr[1]='1';
+    temparr[2]='1';
+    temparr[3]='3';
+    temparr[4]='4';
+
+Serial.println("NFC CALLED");
+    
+    success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, &uid[0], &uidLength, 500);
+ 
+    if (success)
+    {
+        Serial.print("\r\nFound a card!");
+        Serial.print("\r\nUID Length: ");
+        Serial.print(uidLength, DEC);
+        Serial.print(" bytes");
+        Serial.print("\r\nUID Value: ");
+        int a=5;
+        Serial.print("0x4");
+ 
+        for (uint8_t i = 1; i < uidLength; i++)
+        {
+            Serial.print(" 0x");
+            Serial.print(uid[i], HEX);
+            itoa(uid[i],arr,16);
+            temparr[a]=arr[0];
+            temparr[a+1]=arr[1];
+            a=a+2;
+        }
+        Serial.print("\n");
+        Serial.print(temparr);
+        Serial.print("\n");
+        //LoRa.SendMessage(temparr,HEX);
+        Serial.print("sended in hex\n");
+ 
+        // Wait 1 second before continuing
+        //delay(1000);
+    }
+    else
+    {
+        // PN532 probably timed out waiting for a card
+        //Serial.print("\r\nTimed out waiting for a card");
+    }
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
