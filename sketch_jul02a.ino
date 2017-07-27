cf3c2a0fa13a2cfe05ac3120d99a8c21a2ac0983
Park Ju Hyung <qkrwngud825@gmail.com>
2017-07-27 15:15:52 +0900

    TMP
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- sketch_jul02a.ino
+++ sketch_jul02a.ino
@@ -1,9 +1,12 @@
+
 // based on an orginal sketch by Arduino forum member "danigom"
 // http://forum.arduino.cc/index.php?action=profile;u=188950
 
 #include <avr/pgmspace.h>
 #include <LedControl.h>
 #include "font.h"
+#include <LoRaShield.h>
+LoRaShield LoRa(10, 11);
 
 const int numDevices = 4;	// number of MAX7219s used
 const long scrollDelay = 50;	// adjust scrolling speed
@@ -26,22 +29,44 @@ void vib()
 	//delay(200);
 }
 
+void setLPM(bool val) {
+  for (int x = 0; x < numDevices; x++) {
+    lc.shutdown(x, val); //The MAX72XX is in power-saving mode on startup
+    lc.setIntensity(x, 10); // Set the brightness to default value
+    lc.clearDisplay(x); // and clear the display
+  }
+}
+
 void setup()
 {
-	for (int x = 0; x < numDevices; x++) {
-		lc.shutdown(x, false);	//The MAX72XX is in power-saving mode on startup
-		lc.setIntensity(x, 10);	// Set the brightness to default value
-		lc.clearDisplay(x);	// and clear the display
-	}
+  setLPM(true);
 	pinMode(motorPin, OUTPUT);
 
-	//Serial.begin(9600);
+	Serial.begin(115200);
+  LoRa.begin(38400);
+  //LoRa.setTimeout(1);
 }
 
 void loop()
 {
-  vib();
-	showMessage(false);
+  while (LoRa.available()) {
+    String s = LoRa.ReadLine();
+    String m = LoRa.getMessage();
+
+    Serial.print("LoRa : ");
+    Serial.print(s);
+    Serial.print("\n");
+    Serial.print("LoRa Message : ");
+    Serial.print(m);
+    Serial.print("\n");
+
+    if (m == "0001") {
+      setLPM(false);
+      vib();
+      showMessage(false);
+      clearScreen();
+    }
+  }
 	// scrollFont();
 }
 
@@ -77,13 +102,13 @@ int trim_matrix(const unsigned char *font)
 // Show Message
 void showMessage(bool still)
 {
-	int fontsize = sizeof(font5x7) / 8;
+	int fontsize = sizeof(txt1) / 8;
 	for (int counter = 0; counter < fontsize; counter++) {
 		loadBufferLong(counter, still);
 	}
   if (still) {
     printBufferLong();
-    delay(4294967295);
+    delay(4294967295); // Unsigned Long MAX
   }
 }
 
@@ -95,7 +120,7 @@ void loadBufferLong(int offset, bool still)
   // Center align code
   int skip = 0, skip_offset;
  for (a = 0; a < 8; a++) {
-    if (pgm_read_byte_near(font5x7 + (offset * 8) + a) == 0) {
+    if (pgm_read_byte_near(txt1 + (offset * 8) + a) == 0) {
       skip++;
     } else {
       skip_offset = a;
@@ -103,7 +128,7 @@ void loadBufferLong(int offset, bool still)
     }
   }
  for (a = 7; a >= 0; a--) {
-    if (pgm_read_byte_near(font5x7 + (offset * 8) + a) == 0)
+    if (pgm_read_byte_near(txt1 + (offset * 8) + a) == 0)
       skip++;
     else
       break;
@@ -116,7 +141,7 @@ void loadBufferLong(int offset, bool still)
     bufferLong[a * 2] = x;  // Store in buffer
   }
  for (a = skip; a < 8 - skip_offset + 1; a++) { // Loop 7 times for a 5x7 font
-    c = pgm_read_byte_near(font5x7 + (offset * 8) + a - skip + skip_offset); // Index into character table to get row data
+    c = pgm_read_byte_near(txt1 + (offset * 8) + a - skip + skip_offset); // Index into character table to get row data
     x = bufferLong[a * 2];  // Load current scroll buffer
     x = x | c;  // OR the new character onto end of current
     bufferLong[a * 2] = x;  // Store in buffer
@@ -127,16 +152,34 @@ void loadBufferLong(int offset, bool still)
     x = x | c;  // OR the new character onto end of current
     bufferLong[a * 2] = x;  // Store in buffer
   }
-	//byte count = pgm_read_byte_near(font5x7 +((ascii - 0x20) * 9) + 8);     // Index into character table for kerning data
-	int count = trim_matrix(font5x7 + (offset * 8));
-	//byte count = 6;
-	for (a = 0; a < count; a++) {
-		rotateBufferLong();
+  //byte count = pgm_read_byte_near(txt1 +((ascii - 0x20) * 9) + 8);     // Index into character table for kerning data
+  int count = trim_matrix(txt1 + (offset * 8));
+  //byte count = 6;
+  for (a = 0; a < count; a++) {
+    rotateBufferLong();
     if (!still) {
-		  printBufferLong();
-		  delay(scrollDelay);
+      printBufferLong();
+      delay(scrollDelay);
     }
-	}
+  }
+}
+
+void clearScreen(void)
+{
+    int a;
+    unsigned long c, x;
+   for (a = 0; a < 8; a++) { // Loop 7 times for a 5x7 font
+      c = 0;
+      x = bufferLong[a * 2];  // Load current scroll buffer
+      x = x | c;  // OR the new character onto end of current
+      bufferLong[a * 2] = x;  // Store in buffer
+    }
+    int count = 32;
+    for (a = 0; a < count; a++) {
+      rotateBufferLong();
+      printBufferLong();
+      delay(scrollDelay);
+    }
 }
 
 // Rotate the buffer
